a3b2bb46a9c174c4258846f973660f454fd1dfa5
Febri Zuladhari <febrizuladhari12345@gmail.com>
2022-11-23 20:03:43 +0700

    menghilangkan search navbar -febri

--- Sistem Inventory/BinaSwalayan_Inventory/resources/views/components/navbaradmin.blade.php
+++ Sistem Inventory/BinaSwalayan_Inventory/resources/views/components/navbaradmin.blade.php
@@ -9,13 +9,18 @@
 
     <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
         <!-- Search -->
-        <div class="navbar-nav align-items-center">
+        {{-- <div class="navbar-nav align-items-center">
             <div class="nav-item d-flex align-items-center">
                 <i class="bx bx-search-alt bxs-like bx-flashing-hover fs-4 lh-0"></i>
                 <input type="text" class="form-control border-0 shadow-none" placeholder="Search..." aria-label="Search..." />
             </div>
-        </div>
+        </div> --}}
         <!-- /Search -->
+        <div class="navbar-nav align-items-center">
+            <div class="nav-item d-flex align-items-center">
+                <h3 class="my-auto">Bina Swalayan Inventory</h3>
+            </div>
+        </div>
 
         <ul class="navbar-nav flex-row align-items-center ms-auto">
 
