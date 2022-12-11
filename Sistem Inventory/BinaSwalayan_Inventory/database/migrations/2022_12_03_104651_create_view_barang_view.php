7f10c210e95b21ab06c40580dd18f6880f8f0c29
Jo766hi <jonatansilalahi09@gmail.com>
2022-12-11 22:57:44 +0700

    Perbaikan view

--- Sistem Inventory/BinaSwalayan_Inventory/database/migrations/2022_12_03_104651_create_view_barang_view.php
+++ Sistem Inventory/BinaSwalayan_Inventory/database/migrations/2022_12_03_104651_create_view_barang_view.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class CreateViewBarangView extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -12,19 +12,7 @@ class CreateViewBarangView extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE VIEW `view_barang` AS 
-        select 
-        `barangs`.`id` AS `id`,
-        `barangs`.`nama` AS `Nama`,
-        `kategoris`.`id` AS `id_Kategori`,
-        `kategoris`.`nama_kategori` AS `Kategori`,
-        `lokasi_raks`.`id` AS `id_Rak`,
-        `lokasi_raks`.`rak` AS `Rak`,
-        `lokasi_gudangs`.`id` AS `id_Gudang`,
-        `lokasi_gudangs`.`gudang` AS `Gudang`,
-        `outlets`.`id` AS `id_Outlet`,
-        `outlets`.`nama` AS `Outlet`,
-        case when (select exists(select `barangs`.`id` from `perbaikans` where `barangs`.`id` = `perbaikans`.`id_barang` limit 1)) = 1 then 'Perbaikan' when (select exists(select `barangs`.`id` from `laporan_rusaks` where `barangs`.`id` = `laporan_rusaks`.`id_barang` limit 1)) = 1 then 'Rusak' when (select exists(select `barangs`.`id` from `perpindahans` where `barangs`.`id` = `perpindahans`.`id_barang` limit 1)) = 1 then 'Dipinjam' else 'GOOD' end AS `Status` from ((((`kategoris` left join `barangs` on(`barangs`.`id_kategori` = `kategoris`.`id`)) left join `lokasi_raks` on(`barangs`.`id_rak` = `lokasi_raks`.`id`)) left join `lokasi_gudangs` on(`lokasi_raks`.`id_gudang` = `lokasi_gudangs`.`id`)) left join `outlets` on(`lokasi_gudangs`.`id_outlet` = `outlets`.`id`))");
+        DB::statement("CREATE VIEW `view_barang` AS select `barangs`.`id` AS `id`,`barangs`.`nama` AS `Nama`,`kategoris`.`id` AS `id_Kategori`,`kategoris`.`nama_kategori` AS `Kategori`,`lokasi_raks`.`id` AS `id_Rak`,`lokasi_raks`.`rak` AS `Rak`,`lokasi_gudangs`.`id` AS `id_Gudang`,`lokasi_gudangs`.`gudang` AS `Gudang`,`outlets`.`id` AS `id_Outlet`,`outlets`.`nama` AS `Outlet`,case when (select exists(select `barangs`.`id` from `perbaikans` where `barangs`.`id` = `perbaikans`.`id_barang` limit 1)) = 1 then 'Perbaikan' when (select exists(select `barangs`.`id` from `laporan_rusaks` where `barangs`.`id` = `laporan_rusaks`.`id_barang` limit 1)) = 1 then 'Rusak' when (select exists(select `barangs`.`id` from `perpindahans` where `barangs`.`id` = `perpindahans`.`id_barang` limit 1)) = 1 then 'Dipinjam' else 'GOOD' end AS `Status`,`suppliers`.`id` AS `id_Supplier`,`suppliers`.`nama` AS `Supplier` from (((((`barangs` left join `kategoris` on(`barangs`.`id_kategori` = `kategoris`.`id`)) left join `lokasi_raks` on(`barangs`.`id_rak` = `lokasi_raks`.`id`)) left join `lokasi_gudangs` on(`lokasi_raks`.`id_gudang` = `lokasi_gudangs`.`id`)) left join `outlets` on(`lokasi_gudangs`.`id_outlet` = `outlets`.`id`)) left join `suppliers` on(`barangs`.`id_supplier` = `suppliers`.`id`))");
     }
 
     /**
@@ -36,4 +24,4 @@ class CreateViewBarangView extends Migration
     {
         DB::statement("DROP VIEW IF EXISTS `view_barang`");
     }
-}
+};
