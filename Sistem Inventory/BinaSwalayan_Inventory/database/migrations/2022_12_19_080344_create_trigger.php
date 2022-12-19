cb4c2b65df1017b24f3290279ae4b89b0e1ab97d
FairuzArifin <yuuarifin@gmail.com>
2022-12-19 15:11:56 +0700

    Trigger Validasi
    Masih 2 ini Trigger nya, BTW Jangan Lupa Untuk TIDAK Migrate Refresh Melainkan Migrate Path Soalnya Nanti Gabisa di Seed

---
+++ Sistem Inventory/BinaSwalayan_Inventory/database/migrations/2022_12_19_080344_create_trigger.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    
+    public function up()
+    {
+        DB::unprepared('CREATE TRIGGER validasi_level_user BEFORE INSERT ON users
+            FOR EACH ROW
+            BEGIN
+                IF NEW.level = "superadmin"
+                THEN
+                    SIGNAL SQLSTATE "45000" SET MESSAGE_TEXT = "Superadmin Cannot Be Added";
+                END IF;
+            END
+        ');
+        DB::unprepared('CREATE TRIGGER validasi_username BEFORE UPDATE ON users
+            FOR EACH ROW
+            BEGIN
+                IF OLD.username != NEW.username
+                THEN
+                    SIGNAL SQLSTATE "45000" SET MESSAGE_TEXT = "Username Cannot be Edited";
+                END IF;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS validasi_user_level');
+    }
+}
