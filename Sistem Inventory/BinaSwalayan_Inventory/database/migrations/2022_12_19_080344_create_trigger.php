f2d9f966ace7ce1125254c22b6f1b14a9d8656bb
FairuzArifin <yuuarifin@gmail.com>
2022-12-19 15:15:37 +0700

    Tambah 2 Lagi Ah Biar 4 Triggernya
    Liat Deskripsi Sebelumnya

--- Sistem Inventory/BinaSwalayan_Inventory/database/migrations/2022_12_19_080344_create_trigger.php
+++ Sistem Inventory/BinaSwalayan_Inventory/database/migrations/2022_12_19_080344_create_trigger.php
@@ -24,6 +24,27 @@ class CreateTrigger extends Migration
                 END IF;
             END
         ');
+
+        DB::unprepared('CREATE TRIGGER validasi_level_user_2 BEFORE UPDATE ON users
+            FOR EACH ROW
+            BEGIN
+                IF NEW.level = "superadmin"
+                THEN
+                    SIGNAL SQLSTATE "45000" SET MESSAGE_TEXT = "Superadmin Cannot Be Added";
+                END IF;
+            END
+        ');
+
+        DB::unprepared('CREATE TRIGGER validasi_level_user_3 BEFORE DELETE ON users
+            FOR EACH ROW
+            BEGIN
+                IF OLD.level = "superadmin"
+                THEN
+                    SIGNAL SQLSTATE "45000" SET MESSAGE_TEXT = "Superadmin Cannot Be Deleted";
+                END IF;
+            END
+        ');
+        
         DB::unprepared('CREATE TRIGGER validasi_username BEFORE UPDATE ON users
             FOR EACH ROW
             BEGIN
