dfccbb9b47983e40f73e5d347e39b7358547632e
FairuzArifin <yuuarifin@gmail.com>
2022-12-21 09:35:53 +0700

    Perbaikan FPDF
    Daisuki

--- Sistem Inventory/BinaSwalayan_Inventory/app/Http/Controllers/PdfController.php
+++ Sistem Inventory/BinaSwalayan_Inventory/app/Http/Controllers/PdfController.php
@@ -17,7 +17,7 @@ class PdfController extends Controller
 
     public function __construct()
     {
-        $this->fpdf = new Fpdf('p', 'mm', 'Letter');
+        $this->fpdf = new Fpdf('l', 'mm', 'legal');
     }
 
     public function cetakLaporanRusak()
@@ -26,10 +26,11 @@ class PdfController extends Controller
         $this->fpdf->SetFont('Times','B',16);
         $this->fpdf->Cell(0,7,'Damaged Items Report List',0,1,'C');
         $this->fpdf->Cell(10,7,'',0,1);
+        $this->fpdf->SetLeftMargin('30');
         $this->fpdf->SetFont('Times','B',10);
 
         $this->fpdf->Cell(15,6,'ID',1,0,'C');
-        $this->fpdf->Cell(45,6,'Item',1,0,'C');
+        $this->fpdf->Cell(130,6,'Item',1,0,'C');
         $this->fpdf->Cell(30,6,'Category',1,0,'C');
         $this->fpdf->Cell(20,6,'Rack',1,0,'C');
         $this->fpdf->Cell(35,6,'Warehouse',1,0,'C');
@@ -40,7 +41,7 @@ class PdfController extends Controller
         $hasils = View_Rusak::all();
         foreach ($hasils as $hasil){
         $this->fpdf->Cell(15,6,$hasil->id,1,0,'C');
-        $this->fpdf->Cell(45,6,$hasil->nama_barang,1,0,);
+        $this->fpdf->Cell(130,6,$hasil->nama_barang,1,0,);
         $this->fpdf->Cell(30,6,$hasil->kategori,1,0);
         $this->fpdf->Cell(20,6,$hasil->rak,1,0);
         $this->fpdf->Cell(35,6,$hasil->gudang,1,0);
@@ -59,9 +60,10 @@ class PdfController extends Controller
         $this->fpdf->Cell(0,7,'Request Incoming Items',0,1,'C');
         $this->fpdf->Cell(10,7,'',0,1);
         $this->fpdf->SetFont('Times','B',10);
+        $this->fpdf->SetLeftMargin('30');
 
         $this->fpdf->Cell(15,6,'ID',1,0,'C');
-        $this->fpdf->Cell(55,6,'Item',1,0,'C');
+        $this->fpdf->Cell(130,6,'Item',1,0,'C');
         $this->fpdf->Cell(30,6,'Category',1,0,'C');
         $this->fpdf->Cell(45,6,'Requester',1,0,'C');
         $this->fpdf->Cell(50,6,'Outlet',1,1,'C');
@@ -71,7 +73,7 @@ class PdfController extends Controller
         $hasils = View_Req_Pembelian::all();
         foreach ($hasils as $hasil){
         $this->fpdf->Cell(15,6,$hasil->id,1,0,'C');
-        $this->fpdf->Cell(55,6,$hasil->nama_barang,1,0,);
+        $this->fpdf->Cell(130,6,$hasil->nama_barang,1,0,);
         $this->fpdf->Cell(30,6,$hasil->nama_kategori,1,0);
         $this->fpdf->Cell(45,6,$hasil->nama_user,1,0);
         $this->fpdf->Cell(50,6,$hasil->nama_outlet,1,1);
@@ -86,21 +88,20 @@ class PdfController extends Controller
 
     public function cetakListBarang()
     {
-
-
         $this->fpdf->AddPage();
         $this->fpdf->SetFont('Times','B',16);
         $this->fpdf->Cell(0,7,'List of Items in Bina Swalayan',0,1,'C');
         $this->fpdf->Cell(10,7,'',0,1);
+        $this->fpdf->SetLeftMargin('30');
         $this->fpdf->SetFont('Times','B',10);
 
         $this->fpdf->Cell(15,6,'ID',1,0,'C');
-        $this->fpdf->Cell(30,6,'Item',1,0,'C');
+        $this->fpdf->Cell(130,6,'Item',1,0,'C');
         $this->fpdf->Cell(20,6,'Category',1,0,'C');
         $this->fpdf->Cell(10,6,'Rack',1,0,'C');
         $this->fpdf->Cell(20,6,'Warehouse',1,0,'C');
         $this->fpdf->Cell(40,6,'Outlet',1,0,'C');
-        $this->fpdf->Cell(42,6,'Supplier',1,0,'C');
+        $this->fpdf->Cell(50,6,'Supplier',1,0,'C');
         $this->fpdf->Cell(20,6,'Status',1,1,'C');
 
         $this->fpdf->SetFont('Times','',10);
@@ -108,12 +109,12 @@ class PdfController extends Controller
         $hasils = View_Barang::all();
         foreach ($hasils as $hasil){
         $this->fpdf->Cell(15,6,$hasil->id,1,0,'C');
-        $this->fpdf->Cell(30,6,$hasil->Nama,1,0,);
+        $this->fpdf->Cell(130,6,$hasil->Nama,1,0,);
         $this->fpdf->Cell(20,6,$hasil->Kategori,1,0);
         $this->fpdf->Cell(10,6,$hasil->Rak,1,0, 'C');
         $this->fpdf->Cell(20,6,$hasil->Gudang,1,0);
         $this->fpdf->Cell(40,6,$hasil->Outlet,1,0);
-        $this->fpdf->Cell(42,6,$hasil->Supplier,1,0);
+        $this->fpdf->Cell(50,6,$hasil->Supplier,1,0);
         $this->fpdf->Cell(20,6,$hasil->Status,1,1);
 
         }
@@ -130,10 +131,11 @@ class PdfController extends Controller
         $this->fpdf->SetFont('Times','B',16);
         $this->fpdf->Cell(0,7,'Request Shifting Items',0,1,'C');
         $this->fpdf->Cell(10,7,'',0,1);
+        $this->fpdf->SetLeftMargin('30');
         $this->fpdf->SetFont('Times','B',10);
 
         $this->fpdf->Cell(10,6,'ID',1,0,'C');
-        $this->fpdf->Cell(45,6,'Item',1,0,'C');
+        $this->fpdf->Cell(130,6,'Item',1,0,'C');
         $this->fpdf->Cell(30,6,'Category',1,0,'C');
         $this->fpdf->Cell(35,6,'Requester',1,0,'C');
         $this->fpdf->Cell(35,6,'Outlet',1,0,'C');
@@ -144,7 +146,7 @@ class PdfController extends Controller
         $hasils = View_Req_Peminjaman::all();
         foreach ($hasils as $hasil){
         $this->fpdf->Cell(10,6,$hasil->id,1,0,'C');
-        $this->fpdf->Cell(45,6,$hasil->nama_barang,1,0,);
+        $this->fpdf->Cell(130,6,$hasil->nama_barang,1,0,);
         $this->fpdf->Cell(30,6,$hasil->kategori,1,0);
         $this->fpdf->Cell(35,6,$hasil->USER,1,0);
         $this->fpdf->Cell(35,6,$hasil->outlet,1,0);
@@ -166,6 +168,7 @@ class PdfController extends Controller
         $this->fpdf->SetFont('Times','B',16);
         $this->fpdf->Cell(0,7,'List of Items with QR Code',0,1,'C');
         $this->fpdf->Cell(10,7,'',0,1);
+        $this->fpdf->SetLeftMargin('45');
         $this->fpdf->SetFont('Times','B',10);
 
         $this->fpdf->Cell(35,6,'ID',1,0,'C');
